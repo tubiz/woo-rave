ba24068428ac4fea1246a013c0c8412a2572172f
Tunbosun Ayinla <tubiz@users.noreply.github.com>
2024-02-17 18:11:18 +0000

    issue/15 -  Add support for HPOS (#17)

--- woo-rave.php
+++ woo-rave.php
@@ -92,6 +92,15 @@ function tbz_wc_flutterwave_testmode_notice() {
 }
 add_action( 'admin_notices', 'tbz_wc_flutterwave_testmode_notice' );
 
+add_action(
+	'before_woocommerce_init',
+	function() {
+		if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+		}
+	}
+);
+
 /**
  * Registers WooCommerce Blocks integration.
  */
